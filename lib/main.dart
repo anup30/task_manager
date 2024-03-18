9e053864f0cf450796f486fad0df19fa990b6b49
anup30 <anupbarua30@gmail.com>
2024-03-18 20:12:40 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// Task Manager Project
+// new/New in urls.dart file, "status":"new" in add_new_task_screen.
+// new topics:
+// package: pin_code_fields 8.0.1 :
+// A flutter package which will help you to generate pin code fields with beautiful design and animations. Can be useful for OTP or pin code inputs
+// global navigator key
+// shared_preferences
+// image_picker
+
+import 'package:flutter/material.dart';
+import 'package:task_manager/app.dart';
+
+/// solve Data vs data, before assignment submit
+// update_profile_screen: form validation and submit(done)
+// taskStatusCount card size decrease.
+void main(){
+  runApp(const TaskManager());
+}
+
+
+//to do: H.W. recall the home apis after successfully add new task(s) (-done),
+//to do: after successful delete, refresh page without calling api again? completed, progress, cancelled screen HW.
+
+
+// module 15, ui part, classes: 14.2.24, 22.2.24, 27.2.24,
+// module 16 classes, 29.2.24, 3.3.24, 5.3.24
+// module 17 classes, 7.3.24, 10.3.24, 12.3.24
